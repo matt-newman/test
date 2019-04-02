97689cb4a0043062d71f0959488510582144959f
Matt Newman <git@mattnewman.me>
2019-04-02 17:21:52 +0100

    reverting file contents to unformatted version.

--- newsukAmpAdsPreroll.js
+++ newsukAmpAdsPreroll.js
@@ -1,30 +1,22 @@
-videojs.registerPlugin('newsukAmpAdsPreroll', function () {
-
-  if ( !window.AMP ) {
-    return false;
-  }
+videojs.registerPlugin('newsukAmpAdsPreroll', function() {
 
   var myPlayer = this;
 
   function getQuerystring(key, default_) {
-    var regex;
-    var qs;
-
-    if (default_ == null) { 
-      default_ = "";
-    }
-
+    var regex, qs;
+    if (default_ == null) default_ = "";
     key = key.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
     regex = new RegExp("[\\?&]" + key + "=([^&#]*)");
     qs = regex.exec(window.location.href);
-
     if (qs === null) {
-      return default_;
+        return default_;
     } else {
-      return qs[1];
+        return qs[1];
     }
-  }
-  
+}
+
+
   // Get the query string and un-URL encode it
   myPlayer.ima3.settings.serverUrl = myPlayer.ima3.settings.serverUrl + "&iu=" + getQuerystring("iu");
+
 });
