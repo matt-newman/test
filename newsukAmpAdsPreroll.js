8b711f5ebd73759f49827d4cd13907f96a3cc2e7
Matt Newman <git@mattnewman.me>
2019-04-02 17:12:41 +0100

    adding file for testing purposes.

---
+++ newsukAmpAdsPreroll.js
@@ -0,0 +1,30 @@
+videojs.registerPlugin('ampAdsPreroll', function () {
+
+    if ( !window.AMP ) {
+      return false;
+    }
+  
+    var myPlayer = this;
+  
+    function getQuerystring(key, default_) {
+      var regex;
+      var qs;
+  
+      if (default_ == null) { 
+        default_ = "";
+      }
+  
+      key = key.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+      regex = new RegExp("[\\?&]" + key + "=([^&#]*)");
+      qs = regex.exec(window.location.href);
+  
+      if (qs === null) {
+        return default_;
+      } else {
+        return qs[1];
+      }
+    }
+    
+    // Get the query string and un-URL encode it
+    myPlayer.ima3.settings.serverUrl = myPlayer.ima3.settings.serverUrl + "&iu=" + getQuerystring("iu");
+  });
