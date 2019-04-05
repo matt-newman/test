5890ededa57ef9e9bb992804f70587b6d103c0b5
Matt Newman <git@mattnewman.me>
2019-04-05 15:08:49 +0100

    using plugin to alter the description_url used by google ads.

--- nukAmpAdsPreroll.js
+++ nukAmpAdsPreroll.js
@@ -1,6 +1,6 @@
 videojs.registerPlugin('nukAmpAdsPreroll', function () {
 
-  if ( window.location.href.toLowerCase().indexOf('/amp') === -1 ) {
+  if (window.location.href.toLowerCase().indexOf('/amp') === -1) {
     return;
   }
 
@@ -19,7 +19,26 @@ videojs.registerPlugin('nukAmpAdsPreroll', function () {
     }
   }
 
+  function getCanonicalUrl() {
+    //define a variable to hold the canonical URL
+    var url = "https://www.thesun.co.uk/";
+
+    //grab all the link tags in the current page
+    var links = document.getElementsByTagName('link');
+
+    //loop through looking for a canonical link
+    //use the last one found
+    [].slice.apply(links).forEach(function (key) {
+      if (key.rel !== 'canonical') {
+        return;
+      };
+      url = key.href;
+    });
+
+    return encodeURIComponent(url);
+  }
+
   // Get the query string and un-URL encode it
-  myPlayer.ima3.settings.serverUrl = myPlayer.ima3.settings.serverUrl + "&iu=" + getQuerystring("iu");
+  myPlayer.ima3.settings.serverUrl = myPlayer.ima3.settings.serverUrl.replace("&iu=", "&iu=" + getQuerystring("iu")).replace(/&description_url=.*?&/, "&description_url=" + getCanonicalUrl() + '&');
 
 });
